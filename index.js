4dc0e0d4ce7c7f6f178d9031578acc92fd68feef
Thangaraj <thangarasu.graphicdezine@gmail.com>
2023-03-27 13:16:59 +0530

    Deploy version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const cors = require("cors");
 const { PORT } = require("./constant/common");
 const helmet = require("helmet");
